5eb3e42fa69622f05ccf1ac4729aadde61690090
Miles Johnson <mileswjohnson@gmail.com>
2024-08-05 18:49:59 -0700

    Add test config.

--- toolchains/node/src/lib.rs
+++ toolchains/node/src/lib.rs
@@ -1,3 +1,4 @@
+mod config;
 #[cfg(feature = "wasm")]
 mod moon;
 
--- toolchains/node/src/moon.rs
+++ toolchains/node/src/moon.rs
@@ -1,12 +1,15 @@
+use crate::config::NodeConfig;
 use extism_pdk::*;
 use moon_pdk::*;
+use schematic::SchemaBuilder;
 
 #[plugin_fn]
-pub fn register_toolchain(Json(_): Json<ToolMetadataInput>) -> FnResult<Json<ToolMetadataOutput>> {
-    Ok(Json(ToolMetadataOutput {
-        name: NAME.into(),
-        type_of: PluginType::Language,
-        plugin_version: Some(env!("CARGO_PKG_VERSION").into()),
-        ..ToolMetadataOutput::default()
+pub fn register_toolchain(
+    Json(_): Json<ToolchainMetadataInput>,
+) -> FnResult<Json<ToolchainMetadataOutput>> {
+    Ok(Json(ToolchainMetadataOutput {
+        config_schema: Some(SchemaBuilder::build_root::<NodeConfig>()),
+        plugin_version: env!("CARGO_PKG_VERSION").into(),
+        ..ToolchainMetadataOutput::default()
     }))
 }
