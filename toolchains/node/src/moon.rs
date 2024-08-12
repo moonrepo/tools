1483adef70d4979ee6912362810718f366ab1741
Miles Johnson <mileswjohnson@gmail.com>
2024-08-12 18:06:35 +0100

    Update node.

--- toolchains/node/src/moon.rs
+++ toolchains/node/src/moon.rs
@@ -8,7 +8,7 @@ pub fn register_toolchain(
     Json(_): Json<ToolchainMetadataInput>,
 ) -> FnResult<Json<ToolchainMetadataOutput>> {
     Ok(Json(ToolchainMetadataOutput {
-        config_schema: Some(SchemaBuilder::build_root::<NodeConfig>()),
+        // config_schema: Some(SchemaBuilder::build_root::<NodeConfig>()),
         plugin_version: env!("CARGO_PKG_VERSION").into(),
         ..ToolchainMetadataOutput::default()
     }))
